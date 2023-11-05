bc480d1c8544062eb931bac4dbaf035446f4056d
Guan Li <gcl001@ucsd.edu>
2023-11-04 19:26:11 -0700

    button worked...

--- script/script.js
+++ script/script.js
@@ -1,29 +1,9 @@
-// Runs imidiately when you runs
 document.addEventListener("DOMContentLoaded", function() {
-    // Get the active tab's favicon
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      var tab = tabs[0];
-      var tabFavicon = tab.favIconUrl;
-      
-      // Create an image element to display the favicon
-      var faviconImage = document.createElement("img");
-      faviconImage.src = tabFavicon;
-      
-      // Append the image to the popup's body
-      document.body.appendChild(faviconImage);
-    });
-  });
-
-
-//ERROR: Cannot read properties of null.
-
-// Buttons if clicked.
-document.getElementById("myButton").addEventListener("click", function() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      var tab = tabs[0];
-      var tabUrl = tab.url;
-      // Do something with the tabUrl
-      console.log(tab)
-      console.log(tabUrl)
-    });
-  });
+    var button = document.getElementById("b2");
+    if (button) {
+        button.addEventListener("click", function() {
+            console.log("Hello world");
+            // Your other code here
+        });
+    }
+});
