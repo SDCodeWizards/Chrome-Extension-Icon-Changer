b54ed8ea72a996b4fbb24b0e5904e306beabf9f6
Guan Li <gcl001@ucsd.edu>
2023-11-05 17:15:13 -0800

    Update script.js
    - allowes the script to move the current tab over to the front

--- script/script.js
+++ script/script.js
@@ -1,9 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var button = document.getElementById("b2");
-    if (button) {
-        button.addEventListener("click", function() {
-            console.log("Hello world");
-            // Your other code here
-        });
+// chrome.tabs.onActivated.addListener(moveToFirstPosition);
+
+async function moveToFirstPosition() {
+  try {
+    const [currentTab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    if (currentTab) {
+      await chrome.tabs.move(currentTab.id, { index: 0 });
+      console.log("Success.");
     }
+  } catch (error) {
+    if (error.message === "Tabs cannot be edited right now (user may be dragging a tab).") {
+      setTimeout(moveToFirstPosition, 50);
+    } else {
+      console.error(error);
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  var button = document.getElementById("b2");
+  button.addEventListener("click", function() {
+    moveToFirstPosition();
+  });
 });
