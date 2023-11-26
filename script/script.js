47ce2dcdd2a29ca23ffa4500b04bac615c3da027
Guan Li <gcl001@ucsd.edu>
2023-11-25 23:40:30 -0800

    bad push

--- script/script.js
+++ script/script.js
@@ -1,11 +1,9 @@
-// chrome.tabs.onActivated.addListener(moveToFirstPosition);
-
 async function moveToFirstPosition() {
   try {
     const [currentTab] = await chrome.tabs.query({ active: true, currentWindow: true });
     if (currentTab) {
       await chrome.tabs.move(currentTab.id, { index: 0 });
-      console.log("Success.");
+      console.log("Tab moved to the first position.");
     }
   } catch (error) {
     if (error.message === "Tabs cannot be edited right now (user may be dragging a tab).") {
@@ -16,9 +14,36 @@ async function moveToFirstPosition() {
   }
 }
 
+function grabFavIcons() {
+  const faviconElement = document.querySelector("link[rel*='icon']");
+  if (faviconElement) {
+    const favicon = faviconElement.href;
+    console.log('Current tab favicon:', favicon);
+  } else {
+    console.log('Favicon element not found on this page.');
+  }
+}
+
+// contentScript.js
+
+function changeFavicon(newFaviconUrl) {
+  const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+  link.type = 'image/x-icon';
+  link.rel = 'shortcut icon';
+  link.href = newFaviconUrl;
+
+  // Replace the current favicon
+  document.getElementsByTagName('head')[0].appendChild(link);
+}
+
+// Call the function with the URL of your new favicon
+const newFaviconUrl = 'http://www.google.com/favicon.ico';
+// changeTabFavicon(newFaviconUrl);
+
 document.addEventListener("DOMContentLoaded", function() {
   var button = document.getElementById("b2");
   button.addEventListener("click", function() {
-    moveToFirstPosition();
+    // moveToFirstPosition();
+    changeFavicon(newFaviconUrl);
   });
 });
